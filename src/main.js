29ec57252910f0784fbe47563fa493b6ca3ff20e
Dave Gallant <davegallant@gmail.com>
2022-08-22 04:51:41 +0000

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import { createGtm } from "vue-gtm";
 import App from "./App.vue";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,4 +9,10 @@ import "mousetrap/mousetrap.min.js";
 import "./style.css";
 import "./xess.css";
 
-createApp(App).mount("#app");
+const app = createApp(App).mount("#app");
+
+app.use(
+  createGtm({
+    id: "G-YF11ZH9SYD",
+  })
+);
